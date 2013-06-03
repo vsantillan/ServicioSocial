6fb53927a8dee0e2c8f5362d26a91544ae4a1765
jonatan Diaz <m.jonatan.diaz@gmail.com>
2013-06-03 13:31:55 -0500

    altaPlatica

--- ServicioSocial/src/main/java/edu/servicio/toluca/controller/PlaticaController.java
+++ ServicioSocial/src/main/java/edu/servicio/toluca/controller/PlaticaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 /**
  *
- * @author Héctor
+ * @author Jonny
  */
 @Controller
 public class PlaticaController {
