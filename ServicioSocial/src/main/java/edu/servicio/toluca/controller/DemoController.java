dbfd378eb381de4326e615e00299ea15dd43d0fc
Manuel Nieto <manolo7221@hotmail.com>
2013-06-07 13:39:47 -0500

    Correcciones en plantilla de usuario

---
+++ ServicioSocial/src/main/java/edu/servicio/toluca/controller/DemoController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.servicio.toluca.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+/**
+ *
+ * @author bustedvillain
+ */
+@Controller
+public class DemoController {
+    @RequestMapping(method = RequestMethod.GET, value = "/demoAdministrador.do")
+    public String demoAdministrador(Model a)
+    {
+        
+        return "/Plantillas/demoAdministrador";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/demoUsuario.do")
+    public String demoUsuario(Model a)
+    {
+        
+        return "/Plantillas/demoUsuario";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/demoOrganizacion.do")
+    public String demoOrganizacion(Model a)
+    {
+        
+        return "/Plantillas/demoOrganizacion";
+    }
+}
