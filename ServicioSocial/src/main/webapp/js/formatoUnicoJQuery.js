04413561f8d07d7c8ff138e6518adb6cc3ad11f0
Hector Morales Palma <rehoscript@gmail.com>
2013-06-10 11:06:47 -0500

    Reduccion de Codigo

--- ServicioSocial/src/main/webapp/js/formatoUnicoJQuery.js
+++ ServicioSocial/src/main/webapp/js/formatoUnicoJQuery.js
@@ -1,44 +1,16 @@
-$(document).ready(function() {
-    $('#timepicker\\.\\[1\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[2\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[3\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[4\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[5\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[6\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[7\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[8\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[9\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[10\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[11\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[12\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[13\\]').timepicker({
-        showAnim: 'blind'
-    });
-    $('#timepicker\\.\\[14\\]').timepicker({
-        showAnim: 'blind'
-    });
-});
+$(document).on('ready',listo);
+function listo()
+{
+    timePicker();//Inicializa campos JQuery
+    
+}
+function timePicker()
+{
+    var idTimePicker = 14;
+    var idCadena = "";
+    for(var i = 0; i <= idTimePicker; i++) 
+    {
+       idCadena = "#timepicker\\.\\["+i+"\\]";
+       $(idCadena).timepicker({showAnim: 'blind'});
+    }
+}
