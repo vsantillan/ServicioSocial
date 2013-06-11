76380b1a380dc2cfbcf4b8e0b3ef4f28c8127389
Hector Morales Palma <rehoscript@gmail.com>
2013-06-11 11:30:31 -0500

    formatoUnico

--- ServicioSocial/src/main/webapp/js/formatoUnicoJQuery.js
+++ ServicioSocial/src/main/webapp/js/formatoUnicoJQuery.js
@@ -1,9 +1,24 @@
 $(document).on('ready',listo);
+var alumno = {};
 function listo()
 {
     timePicker();//Inicializa campos JQuery
+    $('#loginA').submit(function() {  
+            $("form#frmDatosPersonales :input").each(function(){prepararJSON($(this));
+    });
+    return false;
+    });
+}
+function prepararJSON($atributo)
+{
+    if($atributo.attr ("type")!=="submit")
+    {
+        alumno[$atributo.attr ("name")]=$atributo.val();
+        console.log(alumno);
+    }
     
 }
+
 function timePicker()
 {
     var idTimePicker = 14;
