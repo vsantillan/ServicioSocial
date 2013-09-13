84c9835bbe7741c68b4b56865e69224ee58ab878
Jesus Guzman Mondragon <solosimpi@hotmail.es>
2013-09-13 10:23:42 -0500

    Validando?

--- ServicioSocial/src/main/webapp/js/lugares.js
+++ ServicioSocial/src/main/webapp/js/lugares.js
@@ -1,6 +1,5 @@
 $(document).on('click', ".cambiaStatusLugar", updateLugar);
 $(document).on('click', ".editLugar", loadLugar);
-$(document).on('click', "#envioB", alterLugar);
 
 var id_e;
 
@@ -10,17 +9,16 @@ function loadLugar(e)
     var lugar = $(e.target).attr('id');
     id_e = $(e.target).attr('ide');
     $("#lugar_s").val(lugar);
+    $("input#id").attr("value", id_e);
 }
 
 function alterLugar(e)
 {
     var nuevo_val = $("#lugar_s").val();
-    $.post("editarLugar1.do", {id: id_e , lugar_s:nuevo_val});
-}
-
-function clearLugar(e)
-{
-    $("#lugar").val("");
+    $.post("editarLugar1.do", {id: id_e , lugar_s:nuevo_val},function(response)
+        {
+            
+        });
 }
 
 function updateLugar(e)
@@ -32,7 +30,6 @@ function updateLugar(e)
         var tabla = $('#example').dataTable();
         $.post("cambiaStatusLugar.do", {id: idUpdate}, function(response)
         {
-            tabla.fnDeleteRow(row);
         });
     }
 }
