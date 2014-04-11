247e6569a97f7f6b1418ef0147bd6d14ed204301
jonnyDiaz <m.jonatan.diaz@gmail.com>
2014-04-11 10:30:54 -0500

    correcciones mega merge

--- ServicioSocial/src/main/java/edu/servicio/toluca/controller/BecasController.java
+++ ServicioSocial/src/main/java/edu/servicio/toluca/controller/BecasController.java
@@ -5,10 +5,7 @@
 package edu.servicio.toluca.controller;
 
 import edu.servicio.toluca.beans.Becado;
-<<<<<<< HEAD
 import edu.servicio.toluca.beans.EnviarCorreo;
-=======
->>>>>>> 23651734b1715556bab6015e92394cba5d5ffa05
 import edu.servicio.toluca.beans.Fecha;
 import edu.servicio.toluca.entidades.FormatoUnico;
 import edu.servicio.toluca.sesion.FormatoUnicoFacade;
@@ -32,186 +29,156 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  * @author Jonny
  */
-@Controller
-public class BecasController {
-
-    @EJB(mappedName = "java:global/ServicioSocial/VistaAlumnoFacade")
-    private VistaAlumnoFacade vistaAlumno;
-    @EJB(mappedName = "java:global/ServicioSocial/FormatoUnicoFacade")
-    private FormatoUnicoFacade formatoUnico;
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 23651734b1715556bab6015e92394cba5d5ffa05
-    @RequestMapping(method = RequestMethod.GET, value = "/preseleccionAlumnos.do")
-    public String preseleccionAlumnos(@ModelAttribute(value = "alumnoP") Becado alumnoP, BindingResult result, Model model) throws ParseException {
-        Fecha fecha = new Fecha();
-        List<FormatoUnico> consulta = formatoUnico.findAll();
-        List<FormatoUnico> alumno = new ArrayList<FormatoUnico>();
-        for (int i = 0; i < consulta.size(); i++) {
-            if (((consulta.get(i).getStatusServicio().toString().compareTo("1") == 0) && (consulta.get(i).getTipoServicio().toString().compareTo("1") == 0))) {
-                alumno.add(consulta.get(i));
-            }
-        }
-
-        model.addAttribute("alumno", alumno);
-
-        return "/Becas/preseleccionAlumnos";
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/administracionAlumnosBecados.do")
-    public String administracionAlumnosBecados(@ModelAttribute(value = "alumnoP") Becado alumnoP, BindingResult result, Model model) {
-
-        model.addAttribute("alumno", formatoUnico.findBySpecificField("tipoServicio", "1", "equal", null, null));
-        model.addAttribute("preseleccionado", formatoUnico.findBySpecificField("tipoServicio", "4", "equal", null, null));
-        model.addAttribute("becado", formatoUnico.findBySpecificField("tipoServicio", "3", "equal", null, null));
-        //  model.addAttribute("preseleccionado", formatoUnico.findAll());
-        model.addAttribute("espacio", " ");
-
-        return "/Becas/administracionAlumnosBecados";
-    }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 23651734b1715556bab6015e92394cba5d5ffa05
-    @RequestMapping(method = RequestMethod.GET, value = "/sadf.do")
-    public String correo(Model model) {
-
-        return "/Becas/correo";
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/dbPreseleccionados.xls")
-    public String generaPreseleccionadosExcel(Model model) {
-        return "/Becas/dbPreseleccionadosReporte";
-    }
-<<<<<<< HEAD
-
-    @RequestMapping(method = RequestMethod.GET, value = "/becados.pdf")
-    public String generaPDFBecados(Model model) {
-        return "/Becas/becados";
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/becadosExcel.xls")
-=======
-     @RequestMapping(method = RequestMethod.GET, value = "/becados.pdf")
-    public String generaPDFBecados(Model model) {
-        return "/Becas/becados";
-    }
-      @RequestMapping(method = RequestMethod.GET, value = "/becadosExcel.xls")
->>>>>>> 23651734b1715556bab6015e92394cba5d5ffa05
-    public String generaExcelBecados(Model model) {
-        return "/Becas/dbBecadosReporte";
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/preseleccionadoBD.do")
-<<<<<<< HEAD
-    public @ResponseBody
-    void updateBecados(Becado alumnoP, Model model) {
-        System.out.println("llego aqui");
-        System.out.println(alumnoP.getAlumno());
-=======
-    public  @ResponseBody
-    void updateBecados(Becado alumnoP, Model model) {
-        System.out.println("llego aqui");
-         System.out.println(alumnoP.getAlumno());
->>>>>>> 23651734b1715556bab6015e92394cba5d5ffa05
-        if (alumnoP.getAlumno() != null) {
-            for (String current : alumnoP.getAlumno()) {
-                FormatoUnico form;
-                BigDecimal id = new BigDecimal(current);
-                BigInteger tipoServicio = new BigInteger(String.valueOf(4));
-                form = formatoUnico.find(id);
-                form.setTipoServicio(tipoServicio);
-                formatoUnico.edit(form);
-            }
-//            return "Alumno(s) agregados a la lista de preseleccion de becados";
-//
-//        } else {
-//            return "ERROR no se pudo agregar alumno(s) agregados a la lista de preselelcion de becados";
-//        }
-        }
-<<<<<<< HEAD
-
-//        return "redirect:administracionAlumnosBecados.do";
-=======
-        
-//        return "redirect:administracionAlumnosBecados.do";
-
->>>>>>> 23651734b1715556bab6015e92394cba5d5ffa05
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/quitarAlumno.do")
-    public @ResponseBody
-    String quitarAlumno(@RequestParam(value = "alumno[]", required = false) String[] alumno, Model model) {
-        if (alumno != null) {
-            for (String current : alumno) {
-                FormatoUnico form;
-                //System.out.println(current);
-                BigDecimal id = new BigDecimal(current);
-                BigInteger tipoServicio = new BigInteger(String.valueOf(1));
-                form = formatoUnico.find(id);
-                form.setTipoServicio(tipoServicio);
-                formatoUnico.edit(form);
-            }
-            return "Se a quitado el alumno de la lista de preseleccion";
-
-        } else {
-            return "ERROR no se pudo queitar el alumno de la lista de preseleccion";
-        }
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/enviarCorreo.do")
-    public @ResponseBody
-    String enviarCorreo(@RequestParam(value = "alumno[]", required = false) String[] alumno, Model model) {
-        if (alumno != null) {
-            for (String current : alumno) {
-            }
-            return "Se ha enviado el correo exitosamente";
-
-        } else {
-            return "ERROR no se a podido enviar el correo";
-        }
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/aceptarAlumno.do")
-    public @ResponseBody
-    String aceptarAlumno(@RequestParam(value = "alumno[]", required = false) String[] alumno, Model model) {
-        if (alumno != null) {
-            for (String current : alumno) {
-                FormatoUnico form;
-                BigDecimal id = new BigDecimal(current);
-                BigInteger tipoServicio = new BigInteger(String.valueOf(3));
-                form = formatoUnico.find(id);
-                form.setTipoServicio(tipoServicio);
-                formatoUnico.edit(form);
-            }
-            return "El alumno(s) fue agrado correctamente a la lista de becados";
-
-        } else {
-            return "ERROR no se a podido agregar el alumno a la lista de becados";
-        }
-    }
-
-
-    @RequestMapping(method = RequestMethod.POST, value = "/envioCorreoBecados.do")
-    public @ResponseBody
-    void envioCorreoBecados(@RequestParam(value = "alumno[]", required = false) String[] alumno, String asunto, String descripcion, Model model) {
-        
-        if (alumno != null && asunto != null && descripcion != null) 
-        {            
-            for (String current : alumno) {
-                FormatoUnico form;
-                BigDecimal id = new BigDecimal(current);
-                form = formatoUnico.find(id);
-                if (form.getDatosPersonalesId().getCorreoElectronico()!=null){
-                Thread hiloCorreo = new Thread(new EnviarCorreo(asunto, form.getDatosPersonalesId().getCorreoElectronico(), descripcion));
-                hiloCorreo.start();
-            }
-                
-            }
-
-        }
-    }
-}
+	@Controller
+	public class BecasController {
+	
+	    @EJB(mappedName = "java:global/ServicioSocial/VistaAlumnoFacade")
+	    private VistaAlumnoFacade vistaAlumno;
+	    @EJB(mappedName = "java:global/ServicioSocial/FormatoUnicoFacade")
+	    private FormatoUnicoFacade formatoUnico;
+	
+	    @RequestMapping(method = RequestMethod.GET, value = "/preseleccionAlumnos.do")
+	    public String preseleccionAlumnos(@ModelAttribute(value = "alumnoP") Becado alumnoP, BindingResult result, Model model) throws ParseException {
+	        Fecha fecha = new Fecha();
+	        List<FormatoUnico> consulta = formatoUnico.findAll();
+	        List<FormatoUnico> alumno = new ArrayList<FormatoUnico>();
+	        for (int i = 0; i < consulta.size(); i++) {
+	            if (((consulta.get(i).getStatusServicio().toString().compareTo("1") == 0) && (consulta.get(i).getTipoServicio().toString().compareTo("1") == 0))) {
+	                alumno.add(consulta.get(i));
+	            }
+	        }
+	
+	        model.addAttribute("alumno", alumno);
+	
+	        return "/Becas/preseleccionAlumnos";
+	    }
+	
+	    @RequestMapping(method = RequestMethod.GET, value = "/administracionAlumnosBecados.do")
+	    public String administracionAlumnosBecados(@ModelAttribute(value = "alumnoP") Becado alumnoP, BindingResult result, Model model) {
+	
+	        model.addAttribute("alumno", formatoUnico.findBySpecificField("tipoServicio", "1", "equal", null, null));
+	        model.addAttribute("preseleccionado", formatoUnico.findBySpecificField("tipoServicio", "4", "equal", null, null));
+	        model.addAttribute("becado", formatoUnico.findBySpecificField("tipoServicio", "3", "equal", null, null));
+	        //  model.addAttribute("preseleccionado", formatoUnico.findAll());
+	        model.addAttribute("espacio", " ");
+	
+	        return "/Becas/administracionAlumnosBecados";
+	    }
+	
+	    @RequestMapping(method = RequestMethod.GET, value = "/sadf.do")
+	    public String correo(Model model) {
+	
+	        return "/Becas/correo";
+	    }
+	
+	    @RequestMapping(method = RequestMethod.GET, value = "/dbPreseleccionados.xls")
+	    public String generaPreseleccionadosExcel(Model model) {
+	        return "/Becas/dbPreseleccionadosReporte";
+	    }
+	
+	    @RequestMapping(method = RequestMethod.GET, value = "/becados.pdf")
+	    public String generaPDFBecados(Model model) {
+	        return "/Becas/becados";
+	    }
+	
+	    @RequestMapping(method = RequestMethod.GET, value = "/becadosExcel.xls")
+	    public String generaExcelBecados(Model model) {
+	        return "/Becas/dbBecadosReporte";
+	    }
+	
+	    @RequestMapping(method = RequestMethod.POST, value = "/preseleccionadoBD.do")
+	    public @ResponseBody
+	    void updateBecados(Becado alumnoP, Model model) {
+	        System.out.println("llego aqui");
+	        System.out.println(alumnoP.getAlumno());
+	        if (alumnoP.getAlumno() != null) {
+	            for (String current : alumnoP.getAlumno()) {
+	                FormatoUnico form;
+	                BigDecimal id = new BigDecimal(current);
+	                BigInteger tipoServicio = new BigInteger(String.valueOf(4));
+	                form = formatoUnico.find(id);
+	                form.setTipoServicio(tipoServicio);
+	                formatoUnico.edit(form);
+	            }
+	//            return "Alumno(s) agregados a la lista de preseleccion de becados";
+	//
+	//        } else {
+	//            return "ERROR no se pudo agregar alumno(s) agregados a la lista de preselelcion de becados";
+	//        }
+	        }
+	
+	//        return "redirect:administracionAlumnosBecados.do";
+	    }
+	
+	    @RequestMapping(method = RequestMethod.POST, value = "/quitarAlumno.do")
+	    public @ResponseBody
+	    String quitarAlumno(@RequestParam(value = "alumno[]", required = false) String[] alumno, Model model) {
+	        if (alumno != null) {
+	            for (String current : alumno) {
+	                FormatoUnico form;
+	                //System.out.println(current);
+	                BigDecimal id = new BigDecimal(current);
+	                BigInteger tipoServicio = new BigInteger(String.valueOf(1));
+	                form = formatoUnico.find(id);
+	                form.setTipoServicio(tipoServicio);
+	                formatoUnico.edit(form);
+	            }
+	            return "Se a quitado el alumno de la lista de preseleccion";
+	
+	        } else {
+	            return "ERROR no se pudo queitar el alumno de la lista de preseleccion";
+	        }
+	    }
+	
+	    @RequestMapping(method = RequestMethod.POST, value = "/enviarCorreo.do")
+	    public @ResponseBody
+	    String enviarCorreo(@RequestParam(value = "alumno[]", required = false) String[] alumno, Model model) {
+	        if (alumno != null) {
+	            for (String current : alumno) {
+	            }
+	            return "Se ha enviado el correo exitosamente";
+	
+	        } else {
+	            return "ERROR no se a podido enviar el correo";
+	        }
+	    }
+	
+	    @RequestMapping(method = RequestMethod.POST, value = "/aceptarAlumno.do")
+	    public @ResponseBody
+	    String aceptarAlumno(@RequestParam(value = "alumno[]", required = false) String[] alumno, Model model) {
+	        if (alumno != null) {
+	            for (String current : alumno) {
+	                FormatoUnico form;
+	                BigDecimal id = new BigDecimal(current);
+	                BigInteger tipoServicio = new BigInteger(String.valueOf(3));
+	                form = formatoUnico.find(id);
+	                form.setTipoServicio(tipoServicio);
+	                formatoUnico.edit(form);
+	            }
+	            return "El alumno(s) fue agrado correctamente a la lista de becados";
+	
+	        } else {
+	            return "ERROR no se a podido agregar el alumno a la lista de becados";
+	        }
+	    }
+	
+	    @RequestMapping(method = RequestMethod.POST, value = "/envioCorreoBecados.do")
+	    public @ResponseBody
+	    void envioCorreoBecados(@RequestParam(value = "alumno[]", required = false) String[] alumno, String asunto, String descripcion, Model model) {
+	        
+	        if (alumno != null && asunto != null && descripcion != null) 
+	        {            
+	            for (String current : alumno) {
+	                FormatoUnico form;
+	                BigDecimal id = new BigDecimal(current);
+	                form = formatoUnico.find(id);
+	                if (form.getDatosPersonalesId().getCorreoElectronico()!=null){
+	                Thread hiloCorreo = new Thread(new EnviarCorreo(asunto, form.getDatosPersonalesId().getCorreoElectronico(), descripcion));
+	                hiloCorreo.start();
+	            }
+	                
+	            }
+	
+	        }
+	    }
+	}
--- ServicioSocial/src/main/webapp/js/becados.js
+++ ServicioSocial/src/main/webapp/js/becados.js
@@ -1,189 +1,92 @@
-/* 
- * Funciones del modulo de becados
- */
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    $('#aceptarAlumno').click(function() {
-        var alumno = [];
-        $("input[name='checkbox']:checked").each(function() {
-            alumno.push($(this).val());
-        });
-        console.log(alumno);
-        if (alumno.length === 0)
-        {
-            alert("No se selecciono ningun alumno");
-        }
-        else
-        {
-            $.post("aceptarAlumno.do", {'alumno': alumno}, function(respuesta) {
-                location.reload();
-                alert(respuesta);
-=======
-$('#aceptarAlumno').click(function() {
-    var alumno = [];
-    $("input[name='checkbox']:checked").each(function() {
-        alumno.push($(this).val());
-    });
-    console.log(alumno);
-    if (alumno.length === 0)
-    {
-        alert("No se selecciono ningun alumno");
-    }
-    else
-    {
-        $.post("aceptarAlumno.do", {'alumno': alumno}, function(respuesta) {
-            location.reload();
-            alert(respuesta);
->>>>>>> parent of ae53da8... algo
-
-        });
-    }
-});
-$('#enviarCorreo').click(function() {
-    var alumno = [];
-    $("input[name='checkbox']:checked").each(function() {
-        alumno.push($(this).val());
-    });
-<<<<<<< HEAD
-    $('#enviarCorreo').click(function() {
-        var alumno = [];
-        $("input[name='checkbox']:checked").each(function() {
-            alumno.push($(this).val());
-=======
-$('#aceptarAlumno').click(function() {
-    var alumno = [];
-    $("input[name='checkbox']:checked").each(function() {
-        alumno.push($(this).val());
-    });
-=======
->>>>>>> parent of ae53da8... algo
-    console.log(alumno);
-    if (alumno.length === 0)
-    {
-        alert("No se selecciono ningun alumno");
-    }
-    else
-    {
-<<<<<<< HEAD
-        $.post("aceptarAlumno.do", {'alumno': alumno}, function(respuesta) {
-            location.reload();
-          //  alert(respuesta);
-
-        });
-    }
-});
-
-$('#quitarAlumno').click(function() {
-    var alumno = [];
-    $("input[name='checkbox']:checked").each(function() {
-        alumno.push($(this).val());
-    });
-    console.log(alumno);
-    if (alumno.length === 0)
-    {
-        alert("No se selecciono ningun alumno");
-    }
-    else
-    {
-        $.post("quitarAlumno.do", {'alumno': alumno}, function(respuesta) {
-            location.reload();
-            // alert(respuesta);
-
->>>>>>> a10c6c3907278c50789f6b72a603095aaca73f19
-=======
-        $.post("enviarCorreo.do", {'alumno': alumno}, function(respuesta) {
-            alert(respuesta);
->>>>>>> parent of ae53da8... algo
-        });
-    }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-});
-$('#quitarAlumno').click(function() {
-    var alumno = [];
-    $("input[name='checkbox']:checked").each(function() {
-        alumno.push($(this).val());
->>>>>>> parent of ae53da8... algo
-    });
-    console.log(alumno);
-    if (alumno.length === 0)
-    {
-        alert("No se selecciono ningun alumno");
-    }
-    else
-    {
-        $.post("quitarAlumno.do", {'alumno': alumno}, function(respuesta) {
-            location.reload();
-            // alert(respuesta);
-
-        });
-    }
-
-<<<<<<< HEAD
-    });
-=======
-});
-$('#enviarcorreo1').click(function() {
-    var alumno = [];
-    $("input[name='checkbox']:checked").each(function() {
-        alumno.push($(this).val());
-    });
-    var asunto = $( "#asunto" ).val();
-    var descripcion=$( "#descripcion" ).val();
-    if (alumno.length === 0 || asunto.length == 0 || descripcion.length == 0)
-    {
-        $( "#respuesta" ).addClass("alert alert-danger");
-        $( "#respuesta" ).text( "Error todos los campos son requeridos o no se seleccionaron alumnos" ).show();
-    }
-    else
-    {
-
-        $.post("envioCorreoBecados.do", {'alumno': alumno,'asunto':asunto,'descripcion':descripcion}, function(respuesta) {
-            $( "#respuesta" ).addClass("alert alert-succes");
-            $( "#respuesta" ).text( "Enviado" ).show();
-            location.reload();
-          //  alert(respuesta);
-
-        }); 
-    }
-    
-//    console.log(alumno);
-//    console.log(asunto);
-//    console.log(descripcion);
-});
-
-
-
-$("#preseleccionados").submit(function(event) {
-    parent.location.reload(true);
-});
->>>>>>> a10c6c3907278c50789f6b72a603095aaca73f19
-=======
-});
-//$('#aceptar').click(function() {
-//    $("#preseleccionados").submit();
-//
-//});
-$("#preseleccionados").submit(function(event) {
-    parent.location.reload(true);
-});
->>>>>>> parent of ae53da8... algo
-
-function contar() {
-
-    var checkboxes = preseleccionados.alumno; //Array que contiene los checkbox
-    var cont = 0; //Variable que lleva la cuenta de los checkbox pulsados
-    for (var x = 0; x < checkboxes.length; x++) {
-        if (checkboxes[x].checked) {
-            cont = cont + 1;
-        }
-    }
-    alert("El numero de alumnos seleccionados es: " + cont);
-
-}
-
-
-  
+	/* 
+	 * Funciones del modulo de becados
+	 */
+	
+	$('#aceptarAlumno').click(function() {
+	    var alumno = [];
+	    $("input[name='checkbox']:checked").each(function() {
+	        alumno.push($(this).val());
+	    });
+	    console.log(alumno);
+	    if (alumno.length === 0)
+	    {
+	        alert("No se selecciono ningun alumno");
+	    }
+	    else
+	    {
+	        $.post("aceptarAlumno.do", {'alumno': alumno}, function(respuesta) {
+	            location.reload();
+	          //  alert(respuesta);
+	
+	        });
+	    }
+	});
+	
+	$('#quitarAlumno').click(function() {
+	    var alumno = [];
+	    $("input[name='checkbox']:checked").each(function() {
+	        alumno.push($(this).val());
+	    });
+	    console.log(alumno);
+	    if (alumno.length === 0)
+	    {
+	        alert("No se selecciono ningun alumno");
+	    }
+	    else
+	    {
+	        $.post("quitarAlumno.do", {'alumno': alumno}, function(respuesta) {
+	            location.reload();
+	            // alert(respuesta);
+	
+	        });
+	    }
+	
+	});
+	$('#enviarcorreo1').click(function() {
+	    var alumno = [];
+	    $("input[name='checkbox']:checked").each(function() {
+	        alumno.push($(this).val());
+	    });
+	    var asunto = $( "#asunto" ).val();
+	    var descripcion=$( "#descripcion" ).val();
+	    if (alumno.length === 0 || asunto.length == 0 || descripcion.length == 0)
+	    {
+	        $( "#respuesta" ).addClass("alert alert-danger");
+	        $( "#respuesta" ).text( "Error todos los campos son requeridos o no se seleccionaron alumnos" ).show();
+	    }
+	    else
+	    {
+	        $.post("envioCorreoBecados.do", {'alumno': alumno,'asunto':asunto,'descripcion':descripcion}, function(respuesta) {
+	            $( "#respuesta" ).addClass("alert alert-succes");
+	            $( "#respuesta" ).text( "Enviado" ).show();
+	            location.reload();
+	          //  alert(respuesta);
+	
+	        }); 
+	    }
+	    
+	//    console.log(alumno);
+	//    console.log(asunto);
+	//    console.log(descripcion);
+	});
+	
+	
+	
+	$("#preseleccionados").submit(function(event) {
+	    parent.location.reload(true);
+	});
+	
+	function contar() {
+	
+	    var checkboxes = preseleccionados.alumno; //Array que contiene los checkbox
+	    var cont = 0; //Variable que lleva la cuenta de los checkbox pulsados
+	    for (var x = 0; x < checkboxes.length; x++) {
+	        if (checkboxes[x].checked) {
+	            cont = cont + 1;
+	        }
+	    }
+	    alert("El numero de alumnos seleccionados es: " + cont);
+	
+	}
+	
+	
