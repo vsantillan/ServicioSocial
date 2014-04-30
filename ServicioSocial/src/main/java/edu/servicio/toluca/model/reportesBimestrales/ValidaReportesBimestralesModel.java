69654f589eb2c0915769ac63c09c0a16338c1050
Héctor Morales Palma <rehoscript@gmail.com>
2014-04-30 17:43:49 -0500

    RB

--- ServicioSocial/src/main/java/edu/servicio/toluca/model/reportesBimestrales/ValidaReportesBimestralesModel.java
+++ ServicioSocial/src/main/java/edu/servicio/toluca/model/reportesBimestrales/ValidaReportesBimestralesModel.java
@@ -78,7 +78,8 @@ public class ValidaReportesBimestralesModel {
                                         Exceptions.printStackTrace(ex);
                                     }
                                     System.out.println("fecha sistema" + ahora);
-                                    if (ahora.before(ultimoBimestral.getFechaFin()) && ahora.after(ultimoBimestral.getFechaInicio())) {
+                                    if (ahora.after(ultimoBimestral.getFechaFin())) {
+                                    //if (ahora.before(ultimoBimestral.getFechaFin()) && ahora.after(ultimoBimestral.getFechaInicio())) {
                                         reportesBean.setAccesoFormato(false);
                                         reportesBean.setMensaje("Tu Reporte Bimestral número " + ultimoBimestral.getNumeroReporte() + " fue aceptado. Tienes un total de " + horasServicio + " horas de servicio.Espera al siguiente bimestre para continuar con tu proceso");
                                         reportesBean.setStatus(3);
