a00e7bd02859c7e3bf95c1089fed280c668e12c2
rlr1391 <09280492@ittoluca.edu.mx>
2014-06-02 12:48:39 -0500

    issues

--- ServicioSocial/src/main/java/edu/servicio/toluca/beans/FormatoUnicoDatosPersonalesBean.java
+++ ServicioSocial/src/main/java/edu/servicio/toluca/beans/FormatoUnicoDatosPersonalesBean.java
@@ -49,7 +49,7 @@ public class FormatoUnicoDatosPersonalesBean implements ExpresionesRegulares, Ca
     private String folioDocIdentificacion;
     private boolean acuerdoC;
     @Size(min = 1, max = 100, message = errorBetween+" 1 y 100 para el campo Lugar de Nacimiento")
-    @Pattern(regexp = letrasPrimeroDespuesEspacios, message = errorletras+" en el campo Lugar de Nacimiento")
+    @Pattern(regexp = letrasPrimeroDespuesEspaciosLugar, message = errorletras+" en el campo Lugar de Nacimiento")
     private String lugar_nacimiento;
     private ArrayList<String> listaErrores = new ArrayList<String>();
     MetodosValidacion mv = new MetodosValidacion();
